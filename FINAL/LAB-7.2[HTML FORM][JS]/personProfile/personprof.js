8ed9c4b3f90b2aaf0e314ad7f4293f2ee669476e
Fabliha Hasnin <fabliha.hasnin17@gmail.com>
2023-11-17 01:41:02 +0600

    lab 7.2 [html] [js]

--- FINAL/LAB-7.2[HTML FORM][JS]/personProfile/personprof.js
+++ FINAL/LAB-7.2[HTML FORM][JS]/personProfile/personprof.js
@@ -1,56 +1,83 @@
 function getInfo() {
     let name = document.getElementById('name').value;
+    console.log(name);
     let email = document.getElementById('email').value;
-    let male = document.getElementById('male');
-    let female = document.getElementById('female');
-    let others = document.getElementById('others');
+    let male = document.getElementById('male').checked;
+    let female = document.getElementById('female').checked;
+    let others = document.getElementById('others').checked;
     let gender;
     let dob = document.getElementById("dob").value;
-    let ssc = document.getElementById('ssc');
-    let hsc = document.getElementById('hsc');
-    let bsc = document.getElementById('bsc');
+    let ssc = document.getElementById('ssc').checked;
+    let hsc = document.getElementById('hsc').checked;
+    let bsc = document.getElementById('bsc').checked;
     let degree;
-    let pic= document.getElementById('picture');
+    let bg = document.getElementById('bg').value;
+    let pic = document.getElementById('picture');
 
-
-    if (name === '' || email === '') {
+    if (name === '' || email === '' || dob === '') {
         alert('Empty field!');
-     } 
-     else if (!validName(name)) {
-         alert('name must contain at least two words');
-    } 
-    else if(!email.includes("@") || !email.includes("@")){
+        
+    } else if (!validName(name)) {
+        alert('Name must contain at least two words');
+        
+    } else if (!email.includes("@") || !email.includes(".com")) {
         alert('Email must be valid like abc@gmail.com');
-   }if (!male.checked && !female.checked && !others.checked) {
-    alert('Please select a gender!');
+       
+    } else if (dob === '') {
+        alert("Date of Birth field is empty");
+        
     }
-    else if (male.checked) {
-        gender = 'Male';
-    } else if (female.checked) {
-        gender = 'Female';
-    } else if (others.checked) {
-        gender = 'Others';
-    }else if (dob=='') {
-        alert ("filed is empty");
-    }else if (!ssc.checked && !hsc.checked && !bsc.checked) {
+    
+    document.getElementById("name1").innerHTML = "Name: " + name;
+    document.getElementById("email1").innerHTML = "Email: " + email;
+
+    
+    if (male) {
+        gender = "Male";
+    } else if (female) {
+        gender = "Female";
+    } else if (others) {
+        gender = "Others";
+    }
+
+    if (!gender) {
+        alert('Please select a gender!');
+    }
+
+    document.getElementById("gender").innerHTML = "Gender: " + gender;
+
+    document.getElementById("dob1").innerHTML = "Date of Birth: " + dob;
+
+    document.getElementById("bg1").innerHTML = "Blood Group: " + bg;
+    
+
+    if (ssc) {
+        degree = "SSC";
+    } else if (hsc) {
+        degree = "HSC";
+    } else if (bsc) {
+        degree = "BSc";
+    }
+
+    if (!degree) {
         alert('Please select a degree!');
+        //return; 
     }
-    else if (ssc.checked) {
-        degree = 'SSC';
-    } else if (hsc.checked) {
-        degree = 'HSC';
-    } else if (bsc.checked) {
-        degree = 'BSc';
-    }else if(!pic)
-    {
-        alert("empty file");
+
+    document.getElementById("degree").innerHTML = "Degree: " + degree;
+    console.log(pic);
+
+    if (!pic.files.length) {
+        alert("Please upload a picture");
+        return;
+    } else {
+        console.log(URL.createObjectURL(pic.files[0]));
+        document.getElementById("pic").src = URL.createObjectURL(pic.files[0]);
     }
-    else {
-            alert('Form submitted');
-     }
 }
 
- function validName(name) {
-     let words = name.split(' ');
-     return words.length >= 2;
- }
+function validName(name) {
+    let words = name.split(' ');
+    return words.length >= 2;
+}
+
